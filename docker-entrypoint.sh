5f1130f04fd3fa08f3b732d6fb07cef3f133467f
frederic-blanc <51082879+frederic-blanc@users.noreply.github.com>
2019-06-21 15:54:40 +0200

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,40 @@
+
+
+# Do not forget that reveal.js manage only the following
+#   'index.html'
+#   'css/**'
+#   'js/**'
+#   'lib/**'
+#   'images/**'
+#   'plugin/**'
+#   '**.md'
+
+cd  /slides
+
+for folder  in $(find * -type d                 2> /dev/null | sort); do
+    if [ -d "/reveal.js/${folder}"  ]; then
+        target=$(cd "/reveal.js/${folder}" ; pwd)
+        if [ "${target}" == "$(readlink -f ${target})" ]; then
+            rm -rf  "/reveal.js/${folder}"
+            ln -s   "/slides/${folder}" "/reveal.js/${folder}"
+        fi
+    else
+        ln -s       "/slides/${folder}" "/reveal.js/${folder}"
+    fi
+done
+
+for file    in $(find * -type f ! -name '.*'    2> /dev/null | sort); do
+    if [ -f "/reveal.js/${file}"    ]; then
+        target=$(cd $(dirname "/reveal.js/${file}") ; pwd)
+        if [ "${target}" == "$(readlink -f ${target})" ]; then
+            rm -f   "/reveal.js/${file}"
+            ln -s   "/slides/${file}"    "/reveal.js/${file}"
+        fi
+    else
+        ln -s       "/slides/${file}"    "/reveal.js/${file}"
+    fi
+done
+
+cd /reveal.js
+
+"$@"
